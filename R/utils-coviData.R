f44f78eedf2e5430b56309a69ae32fa9ef6e2174
Jesse Smith <jesse.smith@shelbycountytn.gov>
2020-11-08 10:33:22 -0600

    Update coviData import docs

--- R/utils-coviData.R
+++ R/utils-coviData.R
@@ -1,23 +1,9 @@
-#' Wrappers Around coviData Function
-#'
-#' \code{coviData} is the package for down/loading, munging, and performing
-#' basic analysis on COVID-19 data for Shelby County. It includes functionality
-#' for downloading data from the integrated data tool, which is re-exported in
-#' covidCluster.
-#'
-#' @inheritParams coviData::download_integrated_data
+#' @inherit coviData::download_integrated_data
 #'
 #' @export
 download_integrated_data <- coviData::download_integrated_data
 
-#' Wrappers Around coviData Function
-#'
-#' \code{coviData} is the package for down/loading, munging, and performing
-#' basic analysis on COVID-19 data for Shelby County. It includes functionality
-#' for loading data from the integrated data tool, which is re-exported in
-#' covidCluster.
-#'
-#' @inheritParams coviData::load_integrated_data
+#' @inherit coviData::load_integrated_data
 #'
 #' @export
 load_integrated_data <- coviData::load_integrated_data
