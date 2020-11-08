ea52ec0667d2af2a7b191fd82322535329967b0e
Jesse Smith <jesse.smith@shelbycountytn.gov>
2020-11-07 18:22:30 -0600

    Added tests for size of create_table results

--- tests/testthat/test-table.R
+++ tests/testthat/test-table.R
@@ -1,5 +1,12 @@
 context("Creating Tables")
 
-test_that("table works with one variable", {
-  expect_type(create_table(iris, Sepal.Width), type = "list")
+test_that("create_table works with 1 variable (output is expected size)", {
+  expect_equal(dim(create_table(iris, Species)), expected = c(3, 3))
+})
+
+test_that("create_table works with >1 variables (output is expected size)", {
+  expect_equal(
+    dim(create_table(iris, dplyr::starts_with("Sepal"))),
+    expected = c(57, 3)
+  )
 })
