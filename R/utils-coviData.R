8ff8a6c6d933f8f9aa18feb7ea3afad811fac426
Jesse Smith <jesse.smith@shelbycountytn.gov>
2020-12-01 09:30:25 -0600

    Move coviData imports to reexports

--- R/utils-coviData.R
+++ R/utils-coviData.R
@@ -1,9 +1,10 @@
-#' @inherit coviData::download_integrated_data
+#' @importFrom coviData download_integrated_data
 #'
 #' @export
-download_integrated_data <- coviData::download_integrated_data
+coviData::download_integrated_data
 
-#' @inherit coviData::load_integrated_data
+
+#' @importFrom coviData load_integrated_data
 #'
 #' @export
-load_integrated_data <- coviData::load_integrated_data
+coviData::load_integrated_data
